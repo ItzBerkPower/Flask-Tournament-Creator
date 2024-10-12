559dc1fbc8ba0eafc7cec371518e92646ce04468
Chief_Ace <eraytopal61@hotmail.com>
2024-10-12 22:44:16 +0800

    Miscellaneous small changes

--- test_app.py
+++ test_app.py
@@ -13,6 +13,8 @@ class FlaskTestase(unittest.TestCase):
         result = self.app.get('/')  # Make a GET request to the '/' route
         self.assertEqual(result.status_code, 200)  # Assert that the status code is 200
         self.assertIn(b'Welcome', result.data)  # Check if 'Home' or some expected content is in the page
+        print("homepage")
+
 
 
     # TESTING LOGIN FUNCTIONALITY (FORM SUBMISSIONS)
@@ -34,6 +36,8 @@ class FlaskTestase(unittest.TestCase):
             # Check if username is set in session
             self.assertIn('username', session) # Check if there is a username in the session
             self.assertEqual(session['username'], 'a') # Check if that username is this accounts' one
+            print("login success")
+
 
 
     # Test a failed login (Wrong credentials)
@@ -43,14 +47,14 @@ class FlaskTestase(unittest.TestCase):
 
             # Simulate POST request to /login with incorrect password or incorrect email
             result = self.app.post('/login', data={
-                'email': 'wrongusername',
-                'password': 'wrongpassword'}, follow_redirects=True)
+                'email': 'wrongemail',
+                'password': 'wrongpassword'})
 
             self.assertNotEqual(result.status_code, 302) # Check if the status code is not 302 (Since there shouldn't be a redirect)
             self.assertIn(b'Invalid email or password. Please try again.', result.data) # Check if the error message appears in the response data
 
             self.assertNotIn('username', session) # Check that the session does not contain the username after failed login   
-    
+            print("login fail")
 
 
 
@@ -58,13 +62,16 @@ class FlaskTestase(unittest.TestCase):
     # TESTING IF A USER CAN ACCESS A PROTECTED PAGE (PAGE THEY CAN ONLY ACCESS WHEN LOGGED IN)
 
     # Accessing a protected page without logging in
-    def protected_page_before_login(self):
+    def test_protected_page_before_login(self):
         with self.app as unused:
             self.assertNotIn('username', session) # Check that there is no username in session (User hasn't logged in)
 
             result = self.app.get('/quick_duel', follow_redirects=True) # Access the duels page, which requires logging in
             self.assertEqual(result.status_code, 200) # This time follow the redirects, and check if a success 200 status code, to check that user has been redirected to login page
             self.assertIn(b'You need to log in first.', result.data)  # Ensure the error message is printed
+
+            print("protect before")
+
     
 
     # Accessing a protected page after logging in
@@ -72,17 +79,21 @@ class FlaskTestase(unittest.TestCase):
         self.app.post('/login', data=dict(email='a@hotmail.com', password='a'), follow_redirects=True) # Log in the user (Account already has profile)
         
         # Now try accessing the protected page
-        result = self.app.get('/quick_duel', follow_redirects=True)
+        result = self.app.get('/profile', follow_redirects=True)
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Quick Duel', result.data)  # Ensure the user can access the duels page
+        self.assertIn(b'Your Profile', result.data)  # Ensure the user can access the duels page
 
+        print("protect after")
 
 
     # ACCESSING A NON-EXISTENT WEBPAGE
     
-    def non_existent_page(self):
-        result = self.app.get('/duels') # Any non-existent page can be placed here, I placed duels because most common one
+    def test_non_existent_page(self):
+        result = self.app.get('/register') # Any non-existent page can be placed here, I placed duels because most common one
         self.assertEqual(result.status_code, 404)  # Expecting 'Page Not Found' error
+        print("non_existent")
+
+
 
 
 
